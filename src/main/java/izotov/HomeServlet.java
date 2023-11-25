fe33517f4207f9653b3a434ff18c181ee0a5732d
izotov_a <aleksandr.izotov2020@gmail.com>
2023-11-25 22:28:54 +0300

    Initial commit

--- src/main/java/izotov/HomeServlet.java
+++ src/main/java/izotov/HomeServlet.java
@@ -1,2 +1,34 @@
-package izotov;public class HomeServlet {
+package izotov;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HomeServlet extends HttpServlet {
+    
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        log("Method init");
+    }
+    
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("Method service enter\n");
+        super.service(req, resp);
+        resp.getWriter().write("Method service exit\n");
+    }
+    
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.getWriter().write("Method doGet\n");
+    }
+    
+    @Override
+    public void destroy() {
+        log("Method destroy");
+    }
 }
